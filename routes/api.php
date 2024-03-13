7c585d5b6ded55f06bb6b9ca8b12dc2ff58e8d77
Soleil Rojas <soleilrojas@gmail.com>
2024-03-13 03:34:58 -0600

    Install api

--- routes/api.php
+++ routes/api.php
@@ -4,22 +4,18 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('csrf', function (Request $request) {
-    return response()->noContent();
-})->middleware('web');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::name('auth.')
     ->prefix('auth')
     ->group(function () {
-        Route::post('/sign-in', [AuthController::class, 'signIn'])
-            ->name('signIn');
-
-        Route::post('/sign-out', [AuthController::class, 'signOut'])
-            ->name('signOut')
-            ->middleware('web');
+        Route::post('/token', [AuthController::class, 'getToken'])
+            ->name('getToken');
 
         Route::get('/user', [AuthController::class, 'getUser'])
-            ->name('user')
+            ->name('getUser')
             ->middleware('auth:sanctum');
 
         Route::post('/user', [AuthController::class, 'updateUser'])
@@ -30,7 +26,7 @@ Route::name('auth.')
             ->name('signUp');
 
         Route::post('/request-validation', [AuthController::class, 'requestValidation'])
-            ->name('request_validation')
+            ->name('requestValidation')
             ->middleware('auth:sanctum');
 
         Route::post('/validate', [AuthController::class, 'validate'])
@@ -38,9 +34,9 @@ Route::name('auth.')
             ->middleware('signed');
 
         Route::post('/request-password-reset', [AuthController::class, 'requestPasswordReset'])
-            ->name('request_password_reset');
+            ->name('requestPasswordReset');
 
         Route::post('/reset-password', [AuthController::class, 'resetPassword'])
-            ->name('reset_password')
+            ->name('resetPassword')
             ->middleware('signed');
     });
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('csrf', function (Request $request) {
+    return response()->noContent();
+});
+
+Route::name('auth.')
+    ->prefix('auth')
+    ->group(function () {
+        Route::post('/sign-in', [AuthController::class, 'signIn'])
+            ->name('signIn');
+
+        Route::post('/sign-out', [AuthController::class, 'signOut'])
+            ->name('signOut');
+    });
