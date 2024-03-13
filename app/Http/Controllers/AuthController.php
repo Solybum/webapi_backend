064fd93d0b49b1edb6b26281988b18b31bbadaf4
Soleil Rojas <soleilrojas@gmail.com>
2024-03-13 03:32:33 -0600

    Update auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\SignInRequest;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
@@ -18,14 +19,22 @@ class AuthController extends Controller
         if (Auth::attempt($validated)) {
             return response()->noContent();
         }
-
-        return response()->isSuccessful();
+        return response()->noContent(Response::HTTP_BAD_REQUEST);
     }
 
     public function signOut()
     {
         Auth::logout();
         return response()->noContent();
+    }
+
+    public function getToken(SignInRequest $request)
+    {
+        $validated = $request->validated();
+        if (Auth::attempt($validated)) {
+            // TODO return an API token
+        }
+        return response()->noContent(Response::HTTP_BAD_REQUEST);
     }
 
     public function getUser(Request $request)
