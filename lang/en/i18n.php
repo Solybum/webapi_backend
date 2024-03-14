ca738db1e0a0bcbbe3f114f0a6c20ba6cb7672fd
Soleil Rojas <soleilrojas@gmail.com>
2024-03-13 23:16:22 -0600

    Add translations

---
+++ lang/en/i18n.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'expires' => [
+        'soon' => 'soon',
+        'in_a_few_seconds' => 'in a few seconds',
+        'in_less_than_a_minute' => 'in less than a minute',
+        'in_minutes' => 'in :value minute|in :value minutes',
+        'in_hours' => 'in :value hour|in :value hours',
+        'in_days' => 'in :value day|in :value days',
+    ]
+];
---
+++ lang/es/i18n.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'expires' => [
+        'soon' => 'pronto',
+        'in_a_few_seconds' => 'en unos segundos',
+        'in_less_than_a_minute' => 'en menos de un minuto',
+        'in_minutes' => 'en :value minuto|en :value minutos',
+        'in_hours' => 'en :value hora|en :value horas',
+        'in_days' => 'en :value día|en :value días',
+    ]
+];
